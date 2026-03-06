29251d40ef8f5cec3a71611474bfd28d2420d1d2
Prakrithi14 <mprakrithi84@gmail.com>
2026-03-06 20:05:59 +0530

    product http request

--- Server/Controller/product_conttroller.js
+++ Server/Controller/product_conttroller.js
@@ -3,26 +3,21 @@ const producttable=require("../Models/Product_model")
 
 //create a function
 const addproduct=async(req,res)=>{
-    
-    const addproduct = async (req, res) => {
-  console.log(req.body)
 
-  const { productname, productprice, productquantity } = req.body
-}
-    // try {
-    //     const{productname,productprice,productquantity,productdescription}=req.body;
-    //     const productdetails=new producttable({
-    //         productname,
-    //         productprice,
-    //         productquantity,
-    //         productdescription
-    //     })
-    //     await productdetails.save();
-    //     res.status(201).json({message:"Product addeds uccessfully",pdata:productdetails})
-    // } catch (error) {
-    //     console.log(error)
-    //     res.status(500).json({message:"Server Error",error})
-    // }
+    try {
+        const{productname,productprice,productquantity,productdescription}=req.body;
+        const productdetails=new producttable({
+            productname,
+            productprice,
+            productquantity,
+            productdescription
+        })
+        await productdetails.save();
+        res.status(201).json({message:"Product added uccessfully",pdata:productdetails})
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({message:"Server Error",error})
+    }
 }
 
 module.exports=addproduct
