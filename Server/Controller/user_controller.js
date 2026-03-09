eb9c56f5925a355a7628e1b12a6f3027672b224c
Prakrithi14 <mprakrithi84@gmail.com>
2026-03-09 15:52:56 +0530

    different http requests

--- Server/Controller/user_controller.js
+++ Server/Controller/user_controller.js
@@ -18,5 +18,53 @@ const usertable=require("../Models/User_model")
         res.status(500).json({message:"Server error",error})
     }
 }
+const getuser=async(req,res)=>{
+    try {
+        const getallusers=await usertable.find()
+        console.log(getallusers)        
+        res.status(200).json({message:"User fetched",allusers:getallusers})
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({message:"Server error",error})
+    }
+}
+const getuserbyid=async(req,res)=>{
+    try {
+        const uid=req.params.id
+        const userbyid=await usertable.findById(uid)
+        console.log(userbyid)
+        res.status(200).json({message:"user found",byid:userbyid})
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({message:"Server error",error})
+    }
 
-module.exports=registeruser
+}
+const deleteuser=async(req,res)=>{
+    try {
+    const did=req.params.id
+    const deleteuser=await usertable.findByIdAndDelete(did)
+    console.log(deleteuser)
+    res.status(200).json({message:"user deleted",d_user:deleteuser})
+        
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({message:"Server error",error})
+        
+    }
+}
+const updateuser=async(req,res)=>{
+    try {
+        const {id}=req.params
+        const body=req.body
+        const updateduser=await usertable.findByIdAndUpdate(id,body,{new:true})
+        console.log(updateduser)
+        res.status(201).json({message:"User updated",updatedata:updateduser})
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({message:"Server error",error})
+        
+    }
+}
+
+module.exports={registeruser,getuser,getuserbyid,deleteuser,updateuser}
--- Server/steps.md
+++ Server/steps.md
@@ -1,9 +1,22 @@
 step 1 : creat server folder
 step 2 : create index.js file
 step 3 : open terminal
-npm init (go on click enter button) has a result package.jason file is created
+npm init (go on click enter button) has a result package.json file is created
 step 4 : in terminal
 npm install express
 npm install mongoose
 npm install nodemon 
 npm install cors
+
+express: 
+It is web framework,Used to handle incoming request and to handle the response also it listens to incoming request and respond to them.
+
+mongoose:
+Handles database connection
+
+cors:
+->cross origin resource sharing
+->Used for the security purpose
+
+nodemon:
+this command is used to run the server .Here server runs whenever there is changes in the file
