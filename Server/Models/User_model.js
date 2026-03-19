45a96bfd3005f8fdb9155f7b969d1f5348511ac7
Prakrithi14 <mprakrithi84@gmail.com>
2026-03-19 14:42:50 +0530

    required fields

--- Server/Models/User_model.js
+++ Server/Models/User_model.js
@@ -2,11 +2,11 @@
 const mongoose=require('mongoose')
 //create new schema
 const userschema=new mongoose.Schema({
-    name:{ type:String},
-    email:{type:String},
-    password:{type:String},
-    phone:{type:Number},
-    address:{type:String},
+    name:{ type:String,required:true},
+    email:{type:String,required:true},
+    password:{type:String,required:true},
+    phone:{type:Number,required:true},
+    address:{type:String,required:true},
 })
 module.exports=mongoose.model("User",userschema)
 
